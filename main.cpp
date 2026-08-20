30cf18c1c092ba30a0f465fe280d0adba308c984
lashachubo <lashachubinidz3@gmail.com>
2026-08-21 00:06:31 +0400

    removed temporary files

--- main.cpp
+++ main.cpp
@@ -51,54 +51,67 @@ void set_nonblocking(int fd) {
 }
 
 int main() {
+  
 
-  // create socket
+  // creating a socket
+  // AF_INET = ipv4
+  // SOCK_STREAM = TCP
+  // then we initialize the unique ID to server_fd and if it fails we return
+
   int server_fd = socket(AF_INET, SOCK_STREAM, 0);
   if (server_fd == -1) {
     perror("Socket failed");
     return 1;
   }
 
-  // prevents "Address already in use" errors
+
+  // we make it reusable after restart so we dont have to wait a minute or so 
   int opt = 1;
   setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
 
-  // bind
+  // binds the socket to an address
   sockaddr_in address;
   address.sin_family = AF_INET;
   address.sin_addr.s_addr = INADDR_ANY;
   address.sin_port = htons(PORT);
 
+  // just checks if bind worked
   if (bind(server_fd, (struct sockaddr*)&address, sizeof(address)) < 0) {
     perror("Bind failed");
     return 1;
   }
 
+  // then we start accepting connections
   listen(server_fd, SOMAXCONN);
   set_nonblocking(server_fd);
 
   std::cout << "Redis Clone listening on port " << PORT << "..." << std::endl;
 
+
+  // creates a watchlist that takes all the active users
   int epoll_fd = epoll_create1(0);
   struct epoll_event ev, events[MAX_EVENTS];
-
-  // allow multiple connections
   ev.events = EPOLLIN;
   ev.data.fd = server_fd;
   epoll_ctl(epoll_fd, EPOLL_CTL_ADD, server_fd, &ev);
 
   while (true) {
+    // this basically notifies if anything happened on any server_fd so we dont 
+    // have to constantly check and nfds keeps track of how many times it happened
     int nfds = epoll_wait(epoll_fd, events, MAX_EVENTS, -1);
 
+    // n is the number of clients that sent data and it goes thru it 1 by 1
     for (int n = 0; n < nfds; ++n) {
+      // check if user is already connected   
       if (events[n].data.fd == server_fd) {
+        // creates a new socket for that user
         struct sockaddr_in client_addr;
         socklen_t addrlen = sizeof(client_addr);
         int client_socket = accept(server_fd, (struct sockaddr*)&client_addr, &addrlen);
 
         set_nonblocking(client_socket);
 
-        // add new client to watchlist
+        // add new user to watchlist
         ev.events = EPOLLIN | EPOLLET;
         ev.data.fd = client_socket;
         epoll_ctl(epoll_fd, EPOLL_CTL_ADD, client_socket, &ev);
